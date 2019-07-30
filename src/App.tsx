c98eaa146b1f53886c6e6a31b3fdb54a409cdf00
wuhw <wuHongwei586@gmail.com>
2019-07-30 21:23:55 +0800

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,8 @@ import './App.css';
 const App: React.FC = () => {
   // const name:string = '1'
   return (
-    <div className="App">
-      <div>
-        <p>文件管理</p>
-      </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>我构建的地一个桌面应用</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div className="App App-starter">
+      <div className="starter"></div>
     </div>
   );
 }
